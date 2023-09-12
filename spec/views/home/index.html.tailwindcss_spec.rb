2882d40915a432b2bedb75d67f64adce09e5105a
Ozzie Osmonson <mikeosmonson@gmail.com>
2023-09-12 13:45:17 -0600

    Rubocop refactor

--- spec/views/home/index.html.tailwindcss_spec.rb
+++ spec/views/home/index.html.tailwindcss_spec.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "home/index.html.tailwindcss", type: :view do
-  
+RSpec.describe 'home/index.html.tailwindcss', type: :view do
 end

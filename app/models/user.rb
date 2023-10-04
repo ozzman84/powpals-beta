0d58710115e20beea46a3e86f8de74d5ae3180d2
Ozzie Osmonson <mikeosmonson@gmail.com>
2023-10-04 10:08:39 -0600

    Rubocop refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,16 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name # assuming the user model has a name
-      user.avatar_url = auth.info.image # assuming the user model has an image
+      set_user_attributes(user, auth)
       user.skip_confirmation! # User is already verified with Google.
     end
+  end
+
+  def self.set_user_attributes(user, auth)
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0, 20]
+    user.first_name = auth.info.first_name
+    user.last_name = auth.info.last_name
+    user.avatar_url = auth.info.image
   end
 end

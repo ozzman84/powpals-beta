01236c197c9e83122c36a9e210230a722332e585
Ozzie Osmonson <mikeosmonson@gmail.com>
2023-10-30 21:15:39 -0600

    Rubocop refactor

--- app/controllers/accounts/omniauth_callbacks_controller.rb
+++ app/controllers/accounts/omniauth_callbacks_controller.rb
@@ -1,59 +1,61 @@
 # frozen_string_literal: true
 
-class Accounts::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+module Accounts
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    # You should configure your model like this:
+    # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+    # You should also create an action method in this controller like this:
+    # def twitter
+    # end
 
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
+    # More info at:
+    # https://github.com/heartcombo/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
+    # GET|POST /resource/auth/twitter
+    # def passthru
+    #   super
+    # end
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+    # GET|POST /users/auth/twitter/callback
+    # def failure
+    #   super
+    # end
 
-  # protected
+    # protected
 
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
-  def google
-    account = Account.from_omniauth(auth)
+    # The path used when OmniAuth fails
+    # def after_omniauth_failure_path_for(scope)
+    #   super(scope)
+    # end
+    def google
+      account = Account.from_omniauth(auth)
 
-    if account.present?
-      sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect account, event: :authentication
-    else
-      flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-      redirect_to new_account_session_path
+      if account.present?
+        sign_out_all_scopes
+        flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect account, event: :authentication
+      else
+        flash[:alert] =
+          t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+        redirect_to new_account_session_path
+      end
     end
-  end
 
-  protected
+    protected
 
-  def after_omniauth_failure_path_for(_scope)
-    new_account_session_path
-  end
+    def after_omniauth_failure_path_for(_scope)
+      new_account_session_path
+    end
 
-  def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
-  end
+    def after_sign_in_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope) || root_path
+    end
 
-  private
+    private
 
-  def auth
-    @auth ||= request.env['omniauth.auth']
+    def auth
+      @auth ||= request.env['omniauth.auth']
+    end
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  describe "references" do
+  describe 'references' do
     it { should have_one(:user) }
   end
 end

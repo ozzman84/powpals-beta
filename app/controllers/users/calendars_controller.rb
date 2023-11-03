286479634c4bde5434af2a35e37b10341fa92c4c
Ozzie Osmonson <mikeosmonson@gmail.com>
2023-11-03 14:31:23 -0600

    Add create update destroy for my calendar w/links

--- app/controllers/users/calendars_controller.rb
+++ app/controllers/users/calendars_controller.rb
@@ -1,28 +1,54 @@
-class Users::CalendarsController < ApplicationController
-  before_action :authenticate_account!
+# frozen_string_literal: true
 
-  def show
-  end
+module Users
+  class CalendarsController < ApplicationController
+    before_action :authenticate_account!
 
-  def my_calendar
-    @calendar = current_account.user.calendars.where(lodging_id: params[:lodging_id])
-  end
+    def show; end
 
-  def create
-    puts params
-    @calendar = current_account.user.calendars.new(create_calendar_params)
-
-    if @calendar.save
-      redirect_to my_calendar_users_lodging_calendars_path
-    else
-      puts @calendar.errors.full_messages
-      flash.now[:errors] = @calendar.errors.full_messages
+    def my_calendar
+      @lodging = current_account.user.lodgings.find(params[:lodging_id])
+      @calendar = current_account.user.calendars.where(lodging_id: params[:lodging_id])
     end
-  end
 
-  private
+    def create
+      @calendar = current_account.user.calendars.new(create_calendar_params)
 
-  def create_calendar_params
-    params.require(:calendar).permit(:date, :lodging_id, :status)
+      if @calendar.save
+        redirect_to my_calendar_users_lodging_calendars_path
+      else
+        flash.now[:errors] = @calendar.errors.full_messages
+      end
+    end
+
+    def update
+      @calendar = current_account.user.calendars.find(params[:id])
+
+      if @calendar.update(update_calendar_params)
+        redirect_to my_calendar_users_lodging_calendars_path
+      else
+        flash.now[:errors] = @calendar.errors.full_messages
+      end
+    end
+
+    def destroy
+      @calendar = current_account.user.calendars.find(params[:id])
+
+      if @calendar.destroy
+        redirect_to my_calendar_users_lodging_calendars_path
+      else
+        flash.now[:errors] = @calendar.errors.full_messages
+      end
+    end
+
+    private
+
+    def create_calendar_params
+      params.require(:calendar).permit(:date, :lodging_id, :status)
+    end
+
+    def update_calendar_params
+      params.require(:calendar).permit(:status)
+    end
   end
 end
--- spec/requests/users/calendars_spec.rb
+++ spec/requests/users/calendars_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Users::Calendars", type: :request do
+RSpec.describe 'Users::Calendars', type: :request do
   let(:current_account) { create(:account) }
   let(:user) { create(:user, account: current_account) }
   let(:lodging) { create(:lodging) }
-  let!(:roommate) { Roommate.create(lodging: lodging, user: user) }
+  let!(:roommate) { Roommate.create(lodging:, user:) }
 
-  describe "GET /show" do
+  describe 'GET /show' do
     # it "returns http success" do
     #   get users_lodging_calendars_path(lodging)
     #   # binding.pry
@@ -14,7 +16,7 @@ RSpec.describe "Users::Calendars", type: :request do
     # end
   end
 
-  describe "GET /my_calendar" do
+  describe 'GET /my_calendar' do
     # it "returns http success" do
     #   get my_calendar_users_lodging_calendars_path(lodging)
     #   expect(response).to have_http_status(:success)

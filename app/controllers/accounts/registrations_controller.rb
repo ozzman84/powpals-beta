c55c745bd399ef7a677c8adff5ee35e785dc982d
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2023-11-22 13:52:31 -0700

    Clean up files and print statements

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -89,13 +89,11 @@ module Accounts
 
     # The path used after sign up.
     def after_sign_up_path_for(resource)
-      puts current_account.user
       user_path || super(resource)
     end
 
     # The path used after sign up for inactive accounts.
     def after_inactive_sign_up_path_for(resource)
-      puts current_account.user
       edit_user_path || super(resource)
     end
   end

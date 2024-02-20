1b7a5dd62f0f26e2e86c662078049bd2f0c16f8a
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2024-02-19 21:40:17 -0700

    Add table with skier counts

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -6,4 +6,17 @@ class Resort < ApplicationRecord
   has_many :season_pass_resorts
   has_many :season_passes, through: :season_pass_resorts
   has_many :blackout_days
+
+  def skier_count_for_date(date = Date.today)
+    account_ski_days.where(start_date: date).count
+  end
+
+  def next_10_day_skier_count
+    # start_date = Date.today
+    end_date = 8.days.from_now
+
+    (Date.today..end_date).each_with_object({}) do |date, counts|
+      counts[date] = skier_count_for_date(date)
+    end
+  end
 end
--- spec/factories/account_ski_days.rb
+++ spec/factories/account_ski_days.rb
@@ -3,8 +3,6 @@
 FactoryBot.define do
   factory :account_ski_day do
     status { 'MyString' }
-    start_date { '2024-02-15' }
-    account { nil }
-    resort { nil }
+    start_date { Date.today }
   end
 end
